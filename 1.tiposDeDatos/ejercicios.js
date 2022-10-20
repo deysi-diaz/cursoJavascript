4edcce1ca31cfd721916ec262d6c0f0926c062f8
deysi <deysidias56@gmail.com>
2022-10-20 08:45:23 -0500

    averiguando de javacript

--- 1.tiposDeDatos/ejercicios.js
+++ 1.tiposDeDatos/ejercicios.js
@@ -1,2 +1,4 @@
 //averiguar el metodo de javascript
-//que permite 
+//que permite saver que tipo de dato tengo:
+//otro operador que está disponible a partir de javascript
+// el operador typeof, que devuelve una cadena de texto que describe el tipo del operador que estamos comprobando.
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,4 +1,10 @@
 // averiguar las diferencias entre let y var
+// let te permite declarar variables limitando su alcance (scope) al bloque, declaración,
+// o expresión donde se está usando. 
+
+// var la cual define una variable global o local en una función 
+// sin importar el ámbito del bloque.
+
 //la cocina del codigo
 
 //hosting,scope
